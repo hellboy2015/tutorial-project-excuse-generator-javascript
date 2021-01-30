1a58b7dfd0166dcfdf46bdcab94e633b0e27d39d
Julio Avila <hellboy2015@gmail.com>
2021-01-30 01:38:07 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+function generateExcuse() {
+
+    console.log(who[0]);
+    
+
+
+    return excuse;
+}
+
+//onLoad and set the excuse into the innerHTML of the #excuse HTML element.
